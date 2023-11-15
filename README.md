f65cfe9f07ac08f02c5c797b68617605afdf62c0
niuniu85 <a13627385161@163.com>
2023-11-15 16:14:00 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # shop_proxy
 商品代理
+定制开发Fasebook,tiktop,电销机器人，独立站等
+wathsapp:+86 13627385161
--- app/api/chat-example/route.ts
+++ app/api/chat-example/route.ts
@@ -1,20 +1,22 @@
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
- 
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
 // Can be 'nodejs', but Vercel recommends using 'edge'
-export const runtime = 'edge';
- 
+export const runtime = "edge";
+
 const openai = new OpenAI({
-  apiKey: process.env.OPEN_API_KEY,
+  apiKey:
+    process.env.OPEN_API_KEY ||
+    "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",
 });
 // This method must be named GET
 export async function GET() {
   // Make a request to OpenAI's API based on
   // a placeholder prompt
   const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: "gpt-3.5-turbo",
     stream: true,
-    messages: [{ role: 'user', content: 'Say this is a test.' }],
+    messages: [{ role: "user", content: "Say this is a test." }],
   });
   // Log the response
   for await (const part of response) {
